458dac962c3687a4b00889220b7505ee48a4c73d
romariox <romariox@gmail.com>
2015-10-07 10:56:28 -0300

    Preencher automaticamente o site da faculdade

--- www/core/components/login/main.js
+++ www/core/components/login/main.js
@@ -52,6 +52,10 @@ angular.module('mm.core.login', [])
         url: '/site',
         templateUrl: 'core/components/login/templates/site.html',
         controller: 'mmLoginSiteCtrl'
+		onEnter: function($state) {
+        $state.go('mm_login.credentials', {siteurl: 'moodle.grupoateneu.com.br'});
+    }
+
     })
 
     .state('mm_login.credentials', {
